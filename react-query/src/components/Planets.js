c5d994a4de7dfd4d131d7161b784c320ebea2938
Nam Nguyen <nguyenvietnam2401@gmail.com>
2020-08-01 00:28:09 +1000

    🔥React Query: Pagination and Finsihed serie

--- react-query/src/components/Planets.js
+++ react-query/src/components/Planets.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useQuery } from "react-query";
+import { usePaginatedQuery } from "react-query";
 import Planet from "./Planet";
 
 const fetchPlanets = async (key, page) => {
@@ -9,24 +9,42 @@ const fetchPlanets = async (key, page) => {
 
 const Planets = () => {
   const [page, setPage] = useState(1);
-  const { data, status } = useQuery(["planets", page], fetchPlanets);
+  const { resolvedData, latestData, status } = usePaginatedQuery(
+    ["planets", page],
+    fetchPlanets
+  );
   return (
     <div>
       <h2>Planets</h2>
-      <button onClick={() => setPage(1)}>Page 1</button>
-      <button onClick={() => setPage(2)}>Page 2</button>
-      <button onClick={() => setPage(3)}>Page 3</button>
       {status === "loading" && <div>Loading</div>}
       {status === "error" && <div>Error fetching data</div>}
       {status === "success" && (
-        <div>
-          {data.results.map((planet) => (
-            <Planet key={planet.name} planet={planet} />
-          ))}
-        </div>
+        <>
+          <button
+            onClick={() => setPage((old) => Math.max(old - 1, 1))}
+            disabled={page === 1}
+          >
+            Previous Page
+          </button>
+          <span>{page}</span>
+          <button
+            onClick={() =>
+              setPage((old) =>
+                !latestData || !latestData.next ? old : old + 1
+              )
+            }
+            disabled={!latestData || !latestData.next}
+          >
+            Next Page
+          </button>
+          <div>
+            {resolvedData.results.map((planet) => (
+              <Planet key={planet.name} planet={planet} />
+            ))}
+          </div>
+        </>
       )}
     </div>
   );
 };
-
 export default Planets;
