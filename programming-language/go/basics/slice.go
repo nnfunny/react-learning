5c0d28a9daf134ce7beab132026dfce66e25be8d
Nam Nguyen <nguyenvietnam2401@gmail.com>
2021-01-26 18:34:30 +1100

    ✨ Finished array, slice, struc and pointer in go

---
+++ programming-language/go/basics/slice.go
@@ -0,0 +1,13 @@
+// A slice does not store any data, it just describes a section of an underlying array.
+// Changing the elements of a slice modifies the corresponding elements of its underlying array.
+// Other slices that share the same underlying array will see those changes.
+package main
+
+import "fmt"
+
+func main() {
+	primes := [6]int{2, 3, 5, 7, 11, 13}
+
+	var s []int = primes[1:4]
+	fmt.Println(s)
+}
