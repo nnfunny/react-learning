b15422e18a02f4cd022508059f712ccb157e0907
Nam Nguyen <nguyenvietnam2401@gmail.com>
2021-01-26 23:11:08 +1100

    ✅ Finished the third lesson of Next.js

---
+++ front-end/nextjs/components/Navbar.js
@@ -0,0 +1,16 @@
+const Navbar = () => {
+  return ( 
+    <nav>
+      <div className="logo">
+        <h1>
+          Ninja List
+        </h1>
+        <a href="">Home</a>
+        <a href="">About</a>
+        <a href="">Ninja</a>
+      </div>
+    </nav>
+   );
+}
+ 
+export default Navbar;
