eb753172e8f5733e1a28f2a1ed3411ca1ee2859d
Nam Nguyen <nguyenvietnam2401@gmail.com>
2021-01-29 23:25:19 +1100

    ✅ Finished 2 more Next.js lessons

--- front-end/nextjs/pages/index.js
+++ front-end/nextjs/pages/index.js
@@ -1,15 +1,24 @@
-import Head from "next/head";
-import Footer from "../components/Footer";
-import Navbar from "../components/Navbar";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      <h1>Hello</h1>
+      <h1 className={styles.title}>Hello</h1>
+      <p className={styles.text}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus nulla
+        iste assumenda consequuntur voluptatem? Ipsa aliquam assumenda ullam in,
+        possimus asperiores corporis ea tenetur esse sapiente eveniet. Facilis,
+        sequi fuga.
+      </p>
+      <p className={styles.text}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus nulla
+        iste assumenda consequuntur voluptatem? Ipsa aliquam assumenda ullam in,
+        possimus asperiores corporis ea tenetur esse sapiente eveniet. Facilis,
+        sequi fuga.
+      </p>
       <Link href="/ninjas">
-        <a>See Ninjas Listing</a>
+        <a className={styles.btn}>See Ninjas Listing</a>
       </Link>
     </div>
   );
