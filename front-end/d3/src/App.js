208de2d9c3066f182a88a6aff52a769669f4786b
Nam Nguyen <nguyenvietnam2401@gmail.com>
2021-02-25 00:05:46 +1100

    ✨ learned more about data visualization

--- front-end/d3/src/App.js
+++ front-end/d3/src/App.js
@@ -1,32 +1,35 @@
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import * as d3 from "d3";
 import "./App.css";
 
 const URL =
   "https://gist.githubusercontent.com/nnfunny/3bc979d67e470dd62e7ead2968f90e3c/raw/24b338d4b43b733d2c1a6eb31ccaf936217e1d1f/data_color.csv";
-const width = window.innerWidth;
-const height = window.innerHeight;
-const circleRadius = 30;
-const intialMousePosition = { x: width / 2, y: height / 2 };
+// const width = window.innerWidth;
+// const height = window.innerHeight;
 
 function App() {
-  const [mousePosition, setMousePosition] = useState(intialMousePosition);
+  const [data, setData] = useState(null);
   useEffect(() => {
     const fetchedData = async () => {
-      const response = await fetch(URL);
-      const data = await response.text();
-      console.log(data);
+      const data = await d3.csv(URL);
+      setData(data);
+      console.log(d3.csvFormat(data).length / 1024 + " kB");
+      console.log(data.length + " rows");
+      console.log(data.columns.length + " columns");
     };
     fetchedData();
   }, []);
-  const handleMouseMove = useCallback((event) => {
-    const { clientX, clientY } = event;
-    setMousePosition({ x: clientX, y: clientY });
-  },[setMousePosition]);
-
+  const printMessage = (data) => {
+    let message = "";
+    message += d3.csvFormat(data).length / 1024 + " kB ";
+    message += data.length + " rows ";
+    message += data.columns.length + " columns";
+    return message;
+  };
   return (
-    <svg width={width} height={height} onMouseMove={handleMouseMove}>
-      <circle cx={mousePosition.x} cy={mousePosition.y} r={circleRadius} />
-    </svg>
+    <>
+      <pre>Data is: {data ? printMessage(data) : "Loading"}</pre>
+    </>
   );
 }
 
