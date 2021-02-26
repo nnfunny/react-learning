feee84f7dbfa1ebfabed885f8dd06861989029a2
Nam Nguyen <nguyenvietnam2401@gmail.com>
2021-02-27 00:07:09 +1100

    Learned more about d3.js

--- front-end/d3/src/App.js
+++ front-end/d3/src/App.js
@@ -3,33 +3,52 @@ import * as d3 from "d3";
 import "./App.css";
 
 const URL =
-  "https://gist.githubusercontent.com/nnfunny/3bc979d67e470dd62e7ead2968f90e3c/raw/24b338d4b43b733d2c1a6eb31ccaf936217e1d1f/data_color.csv";
-// const width = window.innerWidth;
-// const height = window.innerHeight;
+  "https://gist.githubusercontent.com/curran/0ac4077c7fc6390f5dd33bf5c06cb5ff/raw/605c54080c7a93a417a3cea93fd52e7550e76500/UN_Population_2019.csv";
 
 function App() {
   const [data, setData] = useState(null);
   useEffect(() => {
     const fetchedData = async () => {
-      const data = await d3.csv(URL);
-      setData(data);
+      const row = (d) => {
+        d.Population = +d["2020"];
+        return d;
+      };
+
+      const data = await d3.csv(URL, row);
+      setData(data.slice(0, 10));
       console.log(d3.csvFormat(data).length / 1024 + " kB");
       console.log(data.length + " rows");
       console.log(data.columns.length + " columns");
     };
     fetchedData();
   }, []);
-  const printMessage = (data) => {
-    let message = "";
-    message += d3.csvFormat(data).length / 1024 + " kB ";
-    message += data.length + " rows ";
-    message += data.columns.length + " columns";
-    return message;
-  };
+
+  if (!data) {
+    return <pre>Loadding...</pre>;
+  }
+
+  const yScale = d3
+    .scaleBand()
+    .domain(data.map((d) => d.Country))
+    .range([0, window.innerHeight]);
+
+  const xScale = d3
+    .scaleLinear()
+    .domain([0, d3.max(data, (d) => d.Population)])
+    .range([0, window.innerWidth]);
+
   return (
-    <>
-      <pre>Data is: {data ? printMessage(data) : "Loading"}</pre>
-    </>
+    <svg width={window.innerWidth} height={window.innerHeight}>
+      {data.map((d, index) => (
+        <rect
+          key={index}
+          x={0}
+          y={yScale(d.Country)}
+          width={xScale(d.Population)}
+          height={yScale.bandwidth()}
+        />
+      ))}
+    </svg>
   );
 }
 
