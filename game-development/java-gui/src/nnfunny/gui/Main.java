169076d1a7e0bebd95167a63b7442f5b575a289e
Nam Nguyen <nguyenvietnam2401@gmail.com>
2021-03-04 22:43:23 +1100

    ✅ Learned java gui

--- game-development/java-gui/src/nnfunny/gui/Main.java
+++ game-development/java-gui/src/nnfunny/gui/Main.java
@@ -1,36 +1,39 @@
 package nnfunny.gui;
 
 import javax.swing.*;
-import javax.swing.border.Border;
 import java.awt.*;
 
 public class Main {
   //  final string ROOT_PATH
   public static void main(String[] args) {
-    ImageIcon image = new ImageIcon("resources/images/icon.png");
-    Border border = BorderFactory.createLineBorder(Color.GREEN, 3);
-    JLabel label = new JLabel();
-    label.setText("Bro, Hi");
-    label.setIcon(image);
-    label.setHorizontalTextPosition(JLabel.CENTER);
-    label.setVerticalTextPosition(JLabel.TOP);
-    label.setForeground(new Color(0x12542));
-    label.setFont(new Font("MV Bol", Font.PLAIN, 20));
-    label.setIconTextGap(10);
-    label.setBackground(Color.BLACK);
-    label.setOpaque(true);
-    label.setBorder(border);
-    label.setVerticalAlignment(JLabel.CENTER);
-    label.setHorizontalAlignment(JLabel.CENTER);
-//    label.setBounds(0, 0, 250, 250);
-
     JFrame frame = new JFrame();
-    frame.add(label);
-    frame.setTitle("GUI JAVA");
-    frame.setSize(420, 430);
-    frame.setVisible(true);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//    frame.setLayout(null);
-//    frame.pack();
+    frame.setSize(500, 500);
+    frame.setLayout(new BorderLayout(10, 10));
+    frame.setVisible(true);
+
+    JPanel panel1 = new JPanel();
+    JPanel panel2 = new JPanel();
+    JPanel panel3 = new JPanel();
+    JPanel panel4 = new JPanel();
+    JPanel panel5 = new JPanel();
+
+    panel1.setBackground(Color.RED);
+    panel2.setBackground(Color.GREEN);
+    panel3.setBackground(Color.YELLOW);
+    panel4.setBackground(Color.MAGENTA);
+    panel5.setBackground(Color.BLUE);
+
+    panel1.setPreferredSize(new Dimension(100, 100));
+    panel2.setPreferredSize(new Dimension(100, 100));
+    panel3.setPreferredSize(new Dimension(100, 100));
+    panel4.setPreferredSize(new Dimension(100, 100));
+    panel5.setPreferredSize(new Dimension(100, 100));
+
+    frame.add(panel1, BorderLayout.NORTH);
+    frame.add(panel2, BorderLayout.WEST);
+    frame.add(panel3, BorderLayout.EAST);
+    frame.add(panel4, BorderLayout.SOUTH);
+    frame.add(panel5, BorderLayout.CENTER);
   }
 }
--- game-development/java-gui/src/nnfunny/gui/MyFrame.java
+++ game-development/java-gui/src/nnfunny/gui/MyFrame.java
@@ -1,18 +1,33 @@
 package nnfunny.gui;
 
 import javax.swing.*;
-import java.awt.*;
 
 public class MyFrame extends JFrame {
-  MyFrame() {
-    this.setTitle("GUI JAVA");
-    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    this.setResizable(false);
-    this.setSize(420, 430);
-    this.setVisible(true);
+  final JButton button;
+  final JLabel label;
 
-    ImageIcon image = new ImageIcon("images/icon.png");
-    this.setIconImage(image.getImage());
-    this.getContentPane().setBackground(new Color(0x123445));
+  MyFrame() {
+    ImageIcon icon = new ImageIcon("resources/images/icon.png");
+    label = new JLabel();
+    label.setIcon(icon);
+    label.setBounds(150, 250, 250, 250);
+    label.setVisible(false);
+
+    button = new JButton();
+    button.setBounds(200, 100, 200, 100);
+    button.setText("I'm a button");
+    button.addActionListener(e -> {
+      System.out.println("Hello");
+      label.setVisible(true);
+    });
+    button.setFocusable(false);
+
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.add(button);
+    this.add(label);
+    this.setLayout(null);
+    this.setTitle("GUI JAVA");
+    this.setSize(750, 750);
+    this.setVisible(true);
   }
 }
