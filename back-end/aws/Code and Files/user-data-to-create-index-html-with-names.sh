e4cd48352afb4220f519f83758021aa9317887e3
Nam Nguyen <nguyenvietnam2401@gmail.com>
2021-03-17 22:49:16 +1100

    learned aws

--- back-end/aws/Code and Files/user-data-to-create-index-html-with-names.sh
+++ back-end/aws/Code and Files/user-data-to-create-index-html-with-names.sh
@@ -4,7 +4,7 @@ yum install httpd -y
 systemctl start httpd
 systemctl enable httpd
 cd /var/www/html
-aws s3 cp s3://YOUR-BUCKET-NAME/names.csv ./
-aws s3 cp s3://YOUR-BUCKET-NAME/index.txt ./
+aws s3 cp s3://dct-user-data-a/names.csv ./
+aws s3 cp s3://dct-user-data-a/index.txt ./
 EC2NAME=`cat ./names.csv|sort -R|head -n 1|xargs`
 sed "s/INSTANCEID/$EC2NAME/" index.txt > index.html
