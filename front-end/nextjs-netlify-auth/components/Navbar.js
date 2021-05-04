080241520468e072e0f2b34ccf1f5aa8e3a4b053
Nam Nguyen <nguyenvietnam2401@gmail.com>
2021-05-04 17:37:31 +1000

    nextjs-netlify-auth: developer log in and log out

--- front-end/nextjs-netlify-auth/components/Navbar.js
+++ front-end/nextjs-netlify-auth/components/Navbar.js
@@ -1,20 +1,34 @@
-import Link from 'next/link'
-import Image from 'next/image'
+import Link from "next/link";
+import Image from "next/image";
+import { useContext } from "react";
+import AuthContext from "../store/authContext";
 
 export default function Navbar() {
+  const { user, login, logout } = useContext(AuthContext);
+
   return (
     <div className="container">
       <nav>
         <Image src="/rupee.png" width={50} height={48} />
         <h1>Gaming Vibes</h1>
         <ul>
-          <li><Link href="/"><a>Home</a></Link></li>
-          <li><Link href="/guides"><a>Guides</a></Link></li>
+          <li>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/guides">
+              <a>Guides</a>
+            </Link>
+          </li>
+          { !user && <li onClick={login} className="btn">Login/Signup</li>}
+          { user && <li onClick={logout} className="btn">Logout</li>}
         </ul>
       </nav>
       <div className="banner">
         <Image src="/banner.png" width={966} height={276} />
       </div>
     </div>
-  )
+  );
 }
