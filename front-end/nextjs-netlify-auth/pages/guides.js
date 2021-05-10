4f8962779bc83cc0d222154d25077a0428a0bd87
Nam Nguyen <nguyenvietnam2401@gmail.com>
2021-05-10 20:48:55 +1000

    nextjs-netlify-auth: render guides based on user context

--- front-end/nextjs-netlify-auth/pages/guides.js
+++ front-end/nextjs-netlify-auth/pages/guides.js
@@ -1,9 +1,30 @@
-import styles from '../styles/Guides.module.css'
+import { useContext, useEffect } from "react";
+import styles from "../styles/Guides.module.css";
+import AuthContext from "../store/authContext";
 
-export default function Guides() {
+const Guides = () => {
+  const { user, authReady } = useContext(AuthContext);
+
+  useEffect(() => {
+    if (authReady) {
+      fetch(
+        "/.netlify/functions/guides",
+        user && {
+          headers: {
+            Authorization: `Bearer ${user.token.token_token}`,
+          },
+        }
+      )
+        .then((res) => res.json())
+        .then((data) => console.log(data));
+    }
+  }, [user, authReady]);
+
   return (
     <div className={styles.guides}>
       <h2>All Guides</h2>
-    </div> 
-  )
-}
+    </div>
+  );
+};
+
+export default Guides;
