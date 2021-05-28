ce89fd1338874639c8159e4f20b9bf490d606fb1
Nam Nguyen <nguyenvietnam2401@gmail.com>
2021-05-29 01:06:57 +1000

    go: learn to return multi values

--- programming-language/go/net-ninja-tutorial/main.go
+++ programming-language/go/net-ninja-tutorial/main.go
@@ -5,10 +5,25 @@ import  (
   "fmt"
 )
 
-func main() {
-  greeting := "Hello there friends"
+func getInitials(n string) (string, string) {
+  s := strings.ToUpper(n)
+  name := strings.Split(s, " ")
 
-  fmt.Println(strings.Contains(greeting, "hello"))
-  fmt.Println(strings.ReplaceAll(greeting, "Hello", "hi"))
-  fmt.Println(strings.Index(greeting, "th"))
+  var initials []string
+
+  for _, value := range name {
+    initials = append(initials, value[:1])
+  }
+
+  if (len(initials) > 1) {
+    return initials[0], initials[1]
+  }
+
+  return initials[0], "_"
+}
+
+func main() {
+  first, second := getInitials("nam nguyen")
+
+  fmt.Println(first, second)
 }
