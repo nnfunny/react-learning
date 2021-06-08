bd8e605e7c234d9737414d0418f29172aa46b1c6
Nam Nguyen <nguyenvietnam2401@gmail.com>
2021-06-09 01:58:31 +1000

    go: get input and switch statement

--- programming-language/go/net-ninja-tutorial/main.go
+++ programming-language/go/net-ninja-tutorial/main.go
@@ -1,16 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func getInput(prompt string, r *bufio.Reader) (string, error) {
+	fmt.Print(prompt)
+	input, err := r.ReadString('\n')
+
+	return strings.TrimSpace(input), err
+}
+
+func createBill() bill {
+	reader := bufio.NewReader(os.Stdin)
+
+	name, _ := getInput("Create a new bill name: ", reader)
+
+	b := newBill(name)
+	fmt.Println("Created the bill - ", b.name)
+
+	return b
+}
+
+func promptOptions(b bill) {
+	reader := bufio.NewReader(os.Stdin)
+
+	opt, _ := getInput("Chose option (a - add item, s - save bill, t - add tip): ", reader)
+
+	switch opt {
+	case "a":
+		name, _ := getInput("Item name: ", reader)
+		price, _ := getInput("Item price: ", reader)
+
+		fmt.Println(name, price)
+	case "t":
+		tip, _ := getInput("Enter tip amout ($): ", reader)
+
+		fmt.Println(tip)
+	case "s":
+		fmt.Println("you chose s")
+	default:
+		fmt.Println("that was not a valid option...")
+		promptOptions(b)
+	}
+}
 
 func main() {
-	mybill := newBill("Nam's bill")
+	mybill := createBill()
+	promptOptions(mybill)
 
-	mybill.addItem("onion soup", 4.50)
-	mybill.addItem("veg pie", 8.95)
-	mybill.addItem("toffee pudding", 4.95)
-	mybill.addItem("coffee", 3.25)
-
-	mybill.updateTip(10)
-
-	fmt.Println(mybill.format())
+	// fmt.Println(mybill.format())
 }
